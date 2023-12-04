c7c5698fbb1fc768c9e2c63c51af3de0543f07e4
Steffen Eißmann <steffen.eissmann@wtl.de>
2023-12-04 14:40:45 +0100

    Door04 - part 2

--- door04/index.ts
+++ door04/index.ts
@@ -21,7 +21,33 @@ const solvePart1 = (values: string[]): number => {
 }
 
 const solvePart2 = (values: string[]): number => {
-    return 0;
+    const cardIndex: number[] = [];
+    cardIndex.length = values.length;
+    cardIndex.fill(1, 0, values.length);
+
+    values.map((line: string, idx: number) => {
+        const [winning, numbers] = line.replace(/Card \d+: /gi, '').split('|');
+
+        const winningNumbers = winning.split(' ');
+
+        const matches = numbers.split(' ').filter((number) => {
+            return number !== '' && winningNumbers.includes(number)
+        });
+
+        if (matches.length === 0) {
+            return;
+        }
+
+        const currentCardIndex = cardIndex[idx];
+
+        for (let i = 1; i <= matches.length; i++) {
+            cardIndex[idx+i] += currentCardIndex;
+        }
+    });
+
+    return cardIndex.reduce((acc: number, value: number) => {
+        return acc + value;
+    }, 0);
 }
 
 
