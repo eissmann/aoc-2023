e83807f087273e158c757bef92856af5562ec7f5
Steffen Eißmann <steffen.eissmann@wtl.de>
2023-12-06 11:25:21 +0100

    Door5 - Small performance fixes

--- door05/index.ts
+++ door05/index.ts
@@ -1,5 +1,4 @@
 import {getFileContent} from "../utils/fileHandling";
-import {setDefaultAutoSelectFamily} from "net";
 
 const inputName = __dirname + '/puzzle-input';
 
@@ -10,7 +9,7 @@ const getSimpleSeeds = (line: string): number[] => {
         .split(' ')
         .filter((seed) => seed !== '')
         .map((seed) => parseInt(seed, 10))
-    ;
+        ;
 }
 
 const getLowestLocation = (values: string[], seeds: number[]): number => {
@@ -48,32 +47,31 @@ const getLowestLocation = (values: string[], seeds: number[]): number => {
         if (line === '') {
             return;
         }
-        const [destination, source, len ] = line.split(' ');
+        const [destination, source, len] = line.split(' ');
         const dest = parseInt(destination, 10);
         const src = parseInt(source, 10);
         const length = parseInt(len, 10);
 
-        mappedLines[type].push({ dest, src, length });
+        mappedLines[type][mappedLines[type].length] = {dest, src, length};
     });
 
     supportedTypes.map((type: string) => {
-        previousValues.map((val:number) => {
+        previousValues.map((val: number) => {
             let pushed = false;
             mappedLines[type].map((line: { dest: number, src: number, length: number }) => {
                 // @ts-ignore
-                if (val >= line.src && val <= line.src+line.length-1) {
+                if (val >= line.src && val <= line.src + line.length - 1) {
                     const offset = val as number - line.src;
-                    currentValues.push(line.dest + offset);
+                    currentValues[currentValues.length] = line.dest + offset;
                     pushed = true;
                 }
             });
 
             if (!pushed) {
-                currentValues.push(val);
+                currentValues[currentValues.length] = val;
             }
 
         })
-        // console.log(type, previousValues, previousValues.length, currentValues.length);
 
         previousValues = [...currentValues];
         currentValues = [];
@@ -88,17 +86,17 @@ const solvePart2 = (values: string[]): number => {
     const seeds = getSimpleSeeds(values[0]);
 
     let range: number[] = [];
-    for (let outerI = 0; outerI < seeds.length; outerI+=2) {
+    for (let outerI = 0; outerI < seeds.length; outerI += 2) {
         let counter = 0;
         console.log('Processing ' + outerI + ' of ' + seeds.length);
-        console.log('Found ' + seeds[outerI+1] + ' new seeds');
-        for (let i = seeds[outerI]; i < seeds[outerI] + seeds[outerI+1]; i++) {
-            range.push(i);
+        console.log('Found ' + seeds[outerI + 1] + ' new seeds');
+        for (let i = seeds[outerI]; i < seeds[outerI] + seeds[outerI + 1]; i++) {
+            range[range.length] = i;
             counter++;
-            if ((counter % 100) === 0) {
+            if ((counter % 1000) === 0) {
                 locationNumber = getLowestLocation(values, range)
                 lowestLocation = lowestLocation > -1 ? Math.min(lowestLocation, locationNumber) : locationNumber;
-                counter % 1000000 === 0 ? console.log(lowestLocation, '.') : null;
+                counter % 1000000 === 0 ? process.stdout.write('.') : null;
 
                 range = [];
             }
